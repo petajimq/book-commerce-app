43e52b4bae4265ba6501a4a5138df5997e90eda2
petajimq <tajio01062001@gmail.com>
2025-07-10 23:42:43 +0900

    err

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -1,16 +1,16 @@
 import prisma from "@/app/lib/prisma";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 //購入履歴検索API
 export async function GET(
-  reqest: Request,
+  reqest: NextRequest,
   { params }: { params: { userId: string } }
 ) {
   const userId = params.userId;
 
   try {
     const purchases = await prisma.purchase.findMany({
-      where: { userId: userId },
+      where: { userId },
     });
 
     return NextResponse.json(purchases);
